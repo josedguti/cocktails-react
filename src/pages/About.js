6cf3ba290842df4727cbfe44bffc3cc98de8393e
Jose Gutierrez <josegutierrez@Joses-Mac-mini.fios-router.home>
2021-06-01 18:30:09 -0400

    finished cocktails api project

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className='section about-section'>
+            <h1 className='section-title'>About Us</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. In provident vitae ipsum molestiae exercitationem aperiam. Ipsum necessitatibus assumenda molestiae, quisquam quam doloremque vitae deserunt dolor cupiditate voluptatem esse, officiis illum.
+            </p>
+        </section>
+    )
+}
+
+export default About
